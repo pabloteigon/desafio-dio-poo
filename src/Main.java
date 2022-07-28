3d458ea2dd5a759b94bc97b7d19ca3963dbc8fa1
Pablo Teixeira Gonçalves <pablo.goncalves@Consulting-for.edenred.com>
2022-07-28 18:09:35 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Descrição curso Java");
+        curso.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
+    }
+}
